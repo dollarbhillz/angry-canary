978d95ef4e2d13b5963e0888e899b8161f54137b
Benjamin Kincaid <bkincaid@redhat.com>
2017-07-21 10:45:33 -0400

    added comments to server and a command to do busy work on the container
    Change-Id: If4c686643f0efe413d65997b53841afa726f204f

--- server.py
+++ server.py
@@ -1,43 +1,57 @@
 from flask import Flask
 from flask import request
-#import os
+import os
 import psutil
 import time
 import logging
 
 app = Flask(__name__)
 
+
+# grab psutil cpu usage metrics
 @app.route('/cpu')
 def get_cpu_stats():
-	return str(psutil.cpu_times())
+    return str(psutil.cpu_times()) + '\n'
 
+
+
+# grab psutil memory metrics 
 @app.route('/memory')
 def get_memory_stats():
-	return str(psutil.virtual_memory())
+    return str(psutil.virtual_memory()) + '\n'
 
+
+
+# grab psutil disk metrics 
+# warning: does not currently work when within a container
+# will return an internal server error 
 @app.route('/disk')
 def get_disk_io():
-	return str(psutil.disk_usage())
+    return str(psutil.disk_usage()) + '\n'
 
+
+# begin writing to log every 10 seconds 
 @app.route('/log')
 def start_log():
-        logging.basicConfig(filename="specs.log", level=logging.INFO)
-	print("writing to log")
-	while(True):
-		time.sleep(10)
+    logging.basicConfig(filename="health.log", level=logging.INFO)
+    print("writing to log")
+    while(True):
+        time.sleep(10)
 
-		cpu_stat = "cpu_times: " + str(psutil.cpu_percent()) + "\n"
-		memory_stats = "virtual_memory_stats: " + str(psutil.virtual_memory()) + "\n"
-		disk_io = "disk_io: " + str(psutil.disk_usage()) + "\n"
+        cpu_stat = "cpu_times: " + str(psutil.cpu_percent()) + "\n"
+        memory_stats = "virtual_memory_stats: " + str(psutil.virtual_memory()) + "\n"
+        #disk_io = "disk_io: " + str(psutil.disk_usage()) + "\n"
 
-		logging.info(cpu_stat)
-		logging.info(memory_stats)
-		logging.info(disk_io)
+        logging.info(cpu_stat)
+        logging.info(memory_stats)
+        #logging.info(disk_io)
 
-#@app.route('/stress')
-#def get_metal():
-#        os.system('stress -c 10 --timeout 60&')
+
+# begin simulating work
+@app.route('/busy_work')
+def busy_work():
+    os.system('stress -c 1&')
+    return "stress currently at 1 process"
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1')
-
+    app.run(host='0.0.0.0')
