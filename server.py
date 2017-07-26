d43268e858524af50508e3dcadb450c929c6a717
Benjamin Kincaid <bkincaid@redhat.com>
2017-07-26 11:54:03 -0400

    Added funcitonality for busy-work
    Change-Id: Ic03f59690f9031bfd87560699515b9dd30a0bbc5

--- server.py
+++ server.py
@@ -8,50 +8,53 @@ import logging
 app = Flask(__name__)
 
 
-# grab psutil cpu usage metrics
+# Returns cpu metrics.
 @app.route('/cpu')
 def get_cpu_stats():
     return str(psutil.cpu_times()) + '\n'
 
 
-
-# grab psutil memory metrics 
+# Returns memory metrics.
 @app.route('/memory')
 def get_memory_stats():
     return str(psutil.virtual_memory()) + '\n'
 
 
-
-# grab psutil disk metrics 
-# warning: does not currently work when within a container
-# will return an internal server error 
+# Returns disk metrics
+# Warning: This does not currently work when within a container and will return
+# an internal server error
 @app.route('/disk')
 def get_disk_io():
     return str(psutil.disk_usage()) + '\n'
 
 
-# begin writing to log every 10 seconds 
+# Writes cpu, and memory metrics to a log file every 10ms. Currently, disk
+# metrics are commented out because they will not work when within a container.
+# This should maybe be another child process?
 @app.route('/log')
 def start_log():
     logging.basicConfig(filename="health.log", level=logging.INFO)
-    print("writing to log")
     while(True):
         time.sleep(10)
 
-        cpu_stat = "cpu_times: " + str(psutil.cpu_percent()) + "\n"
-        memory_stats = "virtual_memory_stats: " + str(psutil.virtual_memory()) + "\n"
-        #disk_io = "disk_io: " + str(psutil.disk_usage()) + "\n"
+        cpu_stats = "cpu_stats: " + str(psutil.cpu_percent()) + "\n"
+        memory_stats = "memory_stats: " + str(psutil.virtual_memory()) + "\n"
+       # disk_stats = "disk_io: " + str(psutil.disk_usage()) + "\n"
 
         logging.info(cpu_stat)
         logging.info(memory_stats)
-        #logging.info(disk_io)
+       # logging.info(disk_io)
 
 
 # begin simulating work
-@app.route('/busy_work')
-def busy_work():
-    os.system('stress -c 1&')
-    return "stress currently at 1 process"
+@app.route('/busy_work/<workers>')
+def busy_work(workers):
+    #process = os.popen("python busy_work.py", 'w', 1)
+    #process.write(workers + '\n')
+    return "currently " + workers + " workers running"
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    process = os.popen("python busy_work.py", 'w', 1)
+    process.write("1\n")
+    app.run(host='127.0.0.1')
