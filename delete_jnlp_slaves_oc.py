ceffac47ed4d9b3c1f508dd8a625bc461a50eace
Benjamin Hills <bhills@redhat.com>
2017-08-18 15:04:12 -0400

    Added script to delete runaway jenkins slave pods
    Change-Id: I224f1d0563506c08f915e7acd39a62855634cb71

---
+++ delete_jnlp_slaves_oc.py
@@ -0,0 +1,14 @@
+# Delete le pods script
+import os
+
+os.system("oc get pods > pods.txt")
+pods = open("pods.txt", 'r')
+
+for lines in pods:
+    line = lines.split()
+    if "jnlp-" in line[0]:
+        os.system("oc delete pod " + line[0])
+
+pods.close()
+
+os.system("rm pods.txt")
