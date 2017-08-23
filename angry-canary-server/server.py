f143b7a2233a2931abbc30c8a22f1fa44338f826
Benjamin Kincaid <bkincaid@redhat.com>
2017-08-23 14:53:17 -0400

    working orm in server and sb_test_script.py populates database test with example data
    Change-Id: Ifd625fccf1687035da558c04960fdd1e4e58a50c

--- angry-canary-server/server.py
+++ angry-canary-server/server.py
@@ -15,87 +15,70 @@ db = SQLAlchemy(app)
 
 # Schema defining results of a particular build
 # Plural?, Camel Case?
-class Build_Results(db.Model):
-    __tablename__='build_results'
+class BuildResult(db.Model):
+    __tablename__='build_result'
 
     id = db.Column(db.Integer, primary_key=True)
 
-    # relates to id of build_runs table
-    build_runs_id = db.Column(db.Integer, db.ForeignKey('build_runs.id'))
-    build_runs = db.relationship('Build_Runs',
-            backref=db.backref('build_results', lazy='dynamic'))
-
-    # relates to id of builder_instances table
-    build_instances_id = db.Column(db.Integer, db.ForeignKey('build_instances.id'))
-    build_instances = db.relationship('Build_Instances',
-            backref=db.backref('build_results', lazy='dynamic'))
-
+    # relationship to builder_instance id
+    build_instance_id = db.Column(db.Integer, db.ForeignKey('build_instance.id'))
 
     success = db.Column(db.Boolean)
 
     reason = db.Column(db.String(50))
 
-
-    def __init__(self, build_runs_id, build_runs, build_instances_id,
-                 build_instances, success, reason):
-
-        self.build_runs_id = build_runs_id
-        self.build_runs = build_runs
-        self.build_instances_id = build_instances
-        self.success = success
-        self.reason = reason
-
     def __repr__(self):
-        return '<build_results %d>' % self.id
+        return '<build_result %d>' % self.id
+
 
 
 # Schema defining data about a particualr run of a build
-class Build_Runs(db.Model):
-    __tablename__='build_runs'
+class BuildRun(db.Model):
+    __tablename__='build_run'
 
     id = db.Column(db.Integer, primary_key=True)
     builder_name = db.Column(db.String(50))
-    start_time = db.Column(db.Integer)
-    end_time = db.Column(db.Integer)
 
-    def __init__(self, builder_name, start_time, end_time):
-        self.builder_name = builder_name
-        self.start_time = start_time
-        self.end_time = end_time
+    #change to db.DateTime type
+    start_time = db.Column(db.DateTime)
+    end_time = db.Column(db.DateTime)
+
+    build_instance = db.relationship('BuildInstance', backref='build_run')
 
     def __repr__(self):
-        return '<build_results %d>' % self.id
+        return '<build_run %d>' % self.id
+
 
 
 # Schema defining data about a particualr instance of a build
-class Build_Instances(db.Model):
-    __tablename__='build_instances'
+class BuildInstance(db.Model):
+    __tablename__='build_instance'
 
     id = db.Column(db.Integer, primary_key=True)
 
-    # relates to id of build_runs table
-    build_run_id = db.Column(db.Integer, db.ForeignKey("build_runs.id"))
-    build_run = db.relationship("Build_Runs", backref=db.backref('build_runs',
-        lazy='dynamic'))
+    # relates to id of build_run table
+    build_run_id = db.Column(db.Integer, db.ForeignKey('build_run.id'))
 
-    podname = db.Column(db.String(50))
-    start_time = db.Column(db.Integer)
-    end_time = db.Column(db.Integer)
 
-    def __init__(self, build_run_id, podname, start_time, end_time):
-        self.build_run_id = build_run_id
-        self.podname = podname
-        self.start_time = start_time
-        self.end_time = end_time
+    pod_name = db.Column(db.String(50))
+
+    #change to db.DateTime type
+    start_time = db.Column(db.DateTime)
+    end_time = db.Column(db.DateTime)
+
+    build_result = db.relationship('BuildResult',
+            backref='build_instance', uselist=False)
 
     def __repr__(self):
-        return '<build_instances %d>' % self.id
+        return '<build_instance %d>' % self.id
 
 
 # Route hit by worker pods on success of busy work
 @app.route('/success')
 def success_query():
     return "nice jorb"
+
+
 
 
 # Route hit by worker pods on failure of busy work
